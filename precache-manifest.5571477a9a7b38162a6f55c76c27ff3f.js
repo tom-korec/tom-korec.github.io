c22c77039bd70f5b7dc5858d88ad10ad8bd5135f
Tomáš Korec <tom.korec7@gmail.com>
2020-10-05 23:13:33 +0200

    init

---
+++ precache-manifest.5571477a9a7b38162a6f55c76c27ff3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e4144829604f932c427506b3dc47845",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0db7e9355cafbba79c3",
+    "url": "/static/css/main.54d17e92.chunk.css"
+  },
+  {
+    "revision": "c37f23c8ef33fdcafb4a",
+    "url": "/static/js/2.d70a472e.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.d70a472e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0db7e9355cafbba79c3",
+    "url": "/static/js/main.6250413d.chunk.js"
+  },
+  {
+    "revision": "864964386b8fbd0a16b5",
+    "url": "/static/js/runtime-main.0fe5df00.js"
+  }
+]);
