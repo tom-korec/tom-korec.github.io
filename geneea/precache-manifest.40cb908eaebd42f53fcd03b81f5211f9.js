4ef561ae71e88f05c440e4e4b8d20e075b612c46
Tomáš Korec <tom.korec7@gmail.com>
2020-10-06 22:21:41 +0200

    geneea project

---
+++ geneea/precache-manifest.40cb908eaebd42f53fcd03b81f5211f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d25749ec28679753717b4819ed8ef47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83bc816b82fe309555a1",
+    "url": "/static/css/main.78afbb8c.chunk.css"
+  },
+  {
+    "revision": "c90fb6eb74d780309ed0",
+    "url": "/static/js/2.31852134.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.31852134.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83bc816b82fe309555a1",
+    "url": "/static/js/main.a921cda5.chunk.js"
+  },
+  {
+    "revision": "105fe3fe7c41e85e2c7c",
+    "url": "/static/js/runtime-main.f6a7284f.js"
+  }
+]);
